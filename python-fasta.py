42d76a2a86c9c560b6547f2a4f72c596811feed9
Julia Gamache <julia.gamache@duke.edu>
2019-11-05 11:15:36 -0500

    Parameterizes name of file

---
+++ python-fasta.py
@@ -0,0 +1,13 @@
+import sys
+
+def read_fasta(filename):
+    seq=''
+    f=open(filename)
+    for line in f:
+        line=line.strip()
+        if not '>' in line:
+            seq=seq+line
+    f.close
+    return seq
+
+print(read_fasta(sys.argv[1]))
