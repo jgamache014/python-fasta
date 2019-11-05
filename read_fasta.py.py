d7a5034127d4d2251352bfe729a25ea3f026bd38
Julia Gamache <julia.gamache@duke.edu>
2019-11-05 10:10:21 -0500

    Removed unnecessary text

--- read_fasta.py.py
+++ read_fasta.py.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 def read_fasta(filename):
     seq = ''
@@ -16,8 +11,6 @@ def read_fasta(filename):
 print(read_fasta('ae.fa'))
 
 
-
-# In[ ]:
 
 
 
