9fa114050ac004f826167c8ef23994c414244b0d
Julia Gamache <julia.gamache@duke.edu>
2019-11-05 10:04:44 -0500

    Removes cruft from Jupyter notebook

---
+++ read_fasta.py.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def read_fasta(filename):
+    seq = ''
+    f = open(filename)
+    for line in f:
+        line = line.strip()
+        if not '>' in line:
+            seq = seq+line
+    f.close()
+    return seq
+print(read_fasta('ae.fa'))
+
+
+
+# In[ ]:
+
+
+
+
